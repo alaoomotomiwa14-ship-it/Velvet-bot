7bb95042ecd784bbd0fb73ece949587913d64e2f
alaoomotomiwa14-ship-it <alaoomotomiwa14@gmail.com>
2026-09-12 14:48:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const m=require('@whiskeysockets/baileys');
-const P=require('pino');
-async function s(){
-const {state,saveCreds}=await m.useMultiFileAuthState('./a1');
-const k=m.default({auth:state,logger:P({level:'silent'}),browser:['Velvet','Chrome','1.0'],markOnlineOnConnect:false});
-k.ev.on('creds.update',saveCreds);
-await new Promise(r=>setTimeout(r,2000));
-if(!k.authState.creds.registered){
-const c=await k.requestPairingCode('2349075197821');
-console.log('PAIRING CODE:'+c);
-}}
-s();
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const P = require('pino');
+async function start() {
+  const { state, saveCreds } = await useMultiFileAuthState('./auth');
+  const sock = makeWASocket({ auth: state, logger: P({ level: 'silent' }) });
+  sock.ev.on('creds.update', saveCreds);
+  await new Promise(r => setTimeout(r, 2000));
+  if (!sock.authState.creds.registered) {
+    const code = await sock.requestPairingCode('2349075197821');
+    console.log('PAIRING CODE:' + code);
+  }
+  sock.ev.on('connection.update', u => {
+    if (u.connection === 'open') console.log('CONNECTED');
+  });
+}
+start();
